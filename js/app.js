e7e4adc44caca0cebb7f35c9ce436dd8aebbdaf9
Pablo Abratte <pabratte@gmail.com>
2016-08-18 12:20:48 -0300

    + Se guardan y se cargan los participantes desde el archivo 'participantes.json' (still buggy) + Generación de numero de participante aleatorio no repetido + Agregado panel de participantes

--- js/app.js
+++ js/app.js
@@ -1,9 +1,14 @@
 var app = angular.module('myApp', []);
+fs = require('fs');
 
 app.controller('ctrlMain', function($scope, $interval) {
     $scope.llegadas = [];
     $scope.horaComienzo;
     $scope.elapsed = "--"
+    $scope.nuevoParticipante = {nombre: '', nro: ''};
+    $scope.nuevo_participante_nombre = '';
+
+
 
     $scope.comenzar = function(key){
       horaComienzo = new Date();
@@ -28,5 +33,53 @@ app.controller('ctrlMain', function($scope, $interval) {
       $scope.llegadas.push({elapsed: $scope.elapsed, name: ''});
       console.log($scope.llegadas)
     }
+
+    $scope.agregarParticipante = function(){
+      if($scope.nuevo_participante_nombre == '') return;
+      $scope.nuevoParticipante.nombre = $scope.nuevo_participante_nombre;
+      $scope.nuevoParticipante.nro = $scope.obtenerNuevoNroParticipante();
+      $scope.participantes.push({nombre: $scope.nuevoParticipante.nombre, nro: $scope.nuevoParticipante.nro});
+      $scope.nuevo_participante_nombre = '';
+      $scope.guardarParticipantes();
+    }
+
+    $scope.obtenerNuevoNroParticipante = function(){
+        var repetido = true;
+        var nro = 0;
+        while(repetido){
+          repetido = false;
+          var nro = Math.floor(Math.random() * 100) + 100;
+          for(var i = 0; i<$scope.participantes.length; i++){
+            if($scope.participantes[i].nro == nro){
+              repetido = true;
+              break;
+            }
+          }
+        }
+        return nro;
+    }
+
+    $scope.guardarParticipantes = function(){
+      fs.writeFile("participantes.json", JSON.stringify($scope.participantes), function(err) {
+          if(err) {
+              alert("ERROR guardando archivo participantes.jdon: "+err);
+          }
+      });
+
+    }
+
+    $scope.cargarParticipantes = function(){
+      fs.readFile('participantes.json', "utf8", function(err, data){
+        if(err) {
+            console.log("ERROR leyendo archivo participantes.json: "+err);
+            $scope.participantes = [];
+        }else{
+            $scope.participantes = JSON.parse(data);
+        }
+        $scope.$apply();
+      });
+
+    }
+    $scope.cargarParticipantes();
 
 });
